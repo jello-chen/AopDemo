e3ad5c08eaf13933dc01546b9d69f703dcd6f967
jello chen <jello_chen@live.com>
2017-10-21 01:08:30 +0800

    Init repo

---
+++ AopLib/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AopLib
+{
+    public class Class1
+    {
+        public async void Test(int i)
+        {
+            Console.WriteLine("1");
+            await Task.Delay(1000);
+            Console.WriteLine("2");
+            await Task.Delay(2000);
+        }
+    }
+}
